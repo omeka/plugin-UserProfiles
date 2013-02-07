ce1ca586404b548d2323ef398081ed7c7c143334
patrickmj <patrickmjchnm@gmail.com>
2013-02-07 13:38:52 -0500

    Gets basic public profile editing working

--- views/shared/profiles/edit.php
+++ views/shared/profiles/edit.php
@@ -1,6 +1,7 @@
 <?php
 queue_css_file('profiles');
 queue_css_file('skeleton');
+
 $css = "
 .field label {
   clear: left;
@@ -29,11 +30,36 @@ $css = "
     font-weight: bold;
 }
 
+.inputs .input-block {
+  margin: 0 0 20px;
+}
+
+.input-block {
+    overflow:auto;
+
+}
+
+.inputs textarea {
+    -moz-box-sizing: border-box;
+    width: 100%;
+}
+textarea {
+    -moz-box-sizing: border-box;
+    border: 1px solid #D8D8D8;
+    border-radius: 0 0 0 0;
+    box-shadow: 0 0 0.375em #D6D6D6 inset;
+    color: #666666;
+    font-size: 14px;
+    padding: 5px 10px;
+    width: 100%;
+}
+
+
 ";
 
-queue_js_file('elements');
 queue_js_file('admin-globals');
-
+queue_js_file('tiny_mce', 'javascripts/vendor/tiny_mce');
+queue_js_file('elements');
 
 queue_css_string($css);
 $head = array('title' => 'Edit Profile', 'content_class' => 'horizontal-nav');
@@ -45,21 +71,24 @@ $types = apply_filters('user_profiles_type', $profile_types);
 //<![CDATA[
 // TinyMCE hates document.ready.
 jQuery(window).load(function () {
+
     Omeka.wysiwyg({
         mode: "none",
         forced_root_block: ""
     });
     // Must run the element form scripts AFTER reseting textarea ids.
     jQuery(document).trigger('omeka:elementformload');
+    Omeka.saveScroll();
 });
 
 jQuery(document).bind('omeka:elementformload', function (event) {
-    console.log('binding');
     Omeka.Elements.makeElementControls(event.target, <?php echo js_escape(url('user-profiles/profiles/element-form')); ?>,'UserProfilesProfile'<?php if ($id = metadata('userprofilesprofile', 'id')) echo ', '.$id; ?>);
     Omeka.Elements.enableWysiwyg(event.target);
 });
 //]]>
 </script>
+
+<div class="container container-twelve">
 <ul id='section-nav' class='navigation tabs'>
 <?php 
 
@@ -81,7 +110,7 @@ echo nav($typesNav, 'user_profiles_types_user_edit');
 
 
 <form method="post" action="">
-<section class="user-profiles-profile seven columns alpha">
+<section id="edit-form" class="seven columns alpha">
 <h1>Edit your <?php echo $userprofilestype->label; ?> profile</h1>
 
 <p class="user-profiles-profile-description">
@@ -97,5 +126,5 @@ echo nav($typesNav, 'user_profiles_types_user_edit');
 </section>
 </form>
 </div>
-
+</div>
 <?php echo foot(); ?>
