2c23f11ef2809f8fb08217cebaf19f14f4ebafd5
patrickmj <patrickmjchnm@gmail.com>
2013-02-18 15:24:31 -0500

    link to a profile type so the current tab works

--- views/shared/link_to_owner_profile.php
+++ views/shared/link_to_owner_profile.php
@@ -3,10 +3,14 @@ if(isset($item)) {
     $owner = $item->getOwner();    
 } 
 
-
+if(!isset($type)) {
+    //get the first type
+    $types = get_db()->getTable('UserProfilesType')->findBy(array(), 1);
+    $type = $types[0];
+}
 
 ?>
 
 <div id="user-profiles-link-to-owner">
-<?php echo $text; ?> <a href="<?php echo url('user-profiles/profiles/user/id/' . $owner->id); ?>"><?php echo $owner->name; ?></a>
+<?php echo $text; ?> <a href="<?php echo url('user-profiles/profiles/user/id/' . $owner->id . '?type=' . $type->id); ?>"><?php echo $owner->name; ?></a>
 </div>
